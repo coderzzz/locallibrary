38eb1d1c92a05dd3a987a9d078d2ba2ff1f56ca3
sam_ios <zhangbo@360kad.com>
2018-08-31 15:30:29 +0800

    first

---
+++ locallibrary/catalog/views.py
@@ -0,0 +1,58 @@
+from django.shortcuts import render
+
+from .models import Book, Author, BookInstance, Genre
+from django.views import generic
+
+def index(request):
+    """
+    View function for home page of site.
+    """
+    # Generate counts of some of the main objects
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    # Available books (status = 'a')
+    num_instances_available = BookInstance.objects.filter(
+        status__exact='a').count()
+    num_authors = Author.objects.count()  # The 'all()' is implied by default.
+
+    # Number of visits to this view, as counted in the session variable.
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits+1
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(
+        request,
+        'index.html',
+        context={'num_books': num_books, 'num_instances': num_instances,
+                 'num_instances_available': num_instances_available, 'num_authors': num_authors, 'num_visits': num_visits},
+    )
+
+
+class BookListView(generic.ListView):
+    model = Book
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get the context
+        context = super(BookListView, self).get_context_data(**kwargs)
+        # Create any data and add it to the context
+        queryset = Book.objects.all()
+        context['book_list'] = queryset
+        return context
+    
+   
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+    def book_detail_view(request, pk):
+      try:
+        book_id = Book.objects.get(pk=pk)
+      except Book.DoesNotExist:
+        raise Http404("Book does not exist")
+
+    #book_id=get_object_or_404(Book, pk=pk)
+
+      return render(
+        request,
+        'catalog/book_detail.html',
+        context={'book': book_id, }
+    )
